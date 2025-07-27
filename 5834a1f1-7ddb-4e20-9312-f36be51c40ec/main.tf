c262b91404fa677c2a5dbac0bf08ec004d9a3c0c
sureshpaulchamy <139575808+sureshpaulchamy@users.noreply.github.com>
2025-07-27 09:27:18 -0500

    IaC changes

---
+++ 5834a1f1-7ddb-4e20-9312-f36be51c40ec/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_s3_bucket" "private_bucket" {
+  bucket = "ops0-demo-bucket_jul2025"
+}
+
+resource "aws_s3_bucket_public_access_block" "private_bucket_access" {
+  bucket = aws_s3_bucket.private_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
---
+++ 5834a1f1-7ddb-4e20-9312-f36be51c40ec/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  description = "The AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+  default     = "ops0-demo-bucket_jul2025"
+}
